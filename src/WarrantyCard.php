f4ecd92812c05a496aad6aebc2a4da8adab7e426
Gergely Kovács <gergo.kovacs@innonic.com>
2019-02-26 19:57:39 +0100

    WIP

--- src/WarrantyCard.php
+++ src/WarrantyCard.php
@@ -6,7 +6,19 @@
  * Time: 22:13
  */
 
+namespace WarrantyCard;
+
+use FPDF;
+
 class WarrantyCard
 {
+    private $pdf;
 
+    private $header;
+
+    private $productList;
+
+    private $emptyProductTable;
+
+    private $fixText;
 }
