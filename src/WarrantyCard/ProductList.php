c6412476a8e6f39f3f7c78a712e1f12edb86af7d
Gergely Kovács <gergo.kovacs@innonic.com>
2019-03-02 19:21:08 +0100

    WIP so much thing

--- src/WarrantyCard/ProductList.php
+++ src/WarrantyCard/ProductList.php
@@ -14,29 +14,29 @@ class ProductList extends AbstractPdf implements IWarrantyCardPart
 
     public function addPart()
     {
-        $this->pdf->writeHTML(
-<<<EOD
-Termékek:<br>
-<table>
-    <tr>
-        <td>Név</td>
-        <td>Garancia</td>
-        <td>Garancia lejárat</td>
-        <td>Cikkszám</td>
-        </tr>
-EOD
-);
+        $html =
+        "Termékek:<br>
+        <table>
+            <tr>
+                <td>Név</td>
+                <td>Garancia</td>
+                <td>Garancia lejárat</td>
+                <td>Cikkszám</td>
+                </tr>"
+        ;
 
         foreach ($this->orderData["orderProducts"]["orderProduct"] as $orderProduct) {
-            $this->pdf->writeHTML("
-            <tr>
-                <td>" .$orderProduct["name"] . "</td>
-                <td>18 hónap</td>
-                <td>KI KELL SZÁMOLNI A DÁTUMOT</td>
-                <td>" .$orderProduct["sku"] . "</td>
-            </tr>");
+            $html .=
+                "<tr>
+                    <td>" .$orderProduct["name"] . "</td>
+                    <td>18 hónap</td>
+                    <td>KI KELL SZÁMOLNI A DÁTUMOT</td>
+                    <td>" .$orderProduct["sku"] . "</td>
+                </tr>"
+            ;
         }
 
-        $this->pdf->writeHTML("</table>");
+        $html .= "</table>";
+        $this->pdf->writeHTML($html);
     }
 }
